4310d6609fce3b3fc3a7292aa5195829cf9cc271
ClientCrash <clientcrashyt@gmail.com>
2021-09-07 22:45:32 +0200

    initial logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let mut numbercreated = 0;
+    let args: Vec<String> = env::args().skip(1).collect();
+    for arg in args {
+        println!("Creating {}", arg);
+        //TODO: CREATE FILE
+        numbercreated+=1;
+    }
+    println!("Done. Created {} files.",numbercreated);
 }
