bc539cc54dbd4ea7f2c15844704a2dd9c2378d1e
ClientCrash <clientcrashyt@gmail.com>
2021-09-08 16:43:51 +0200

    file creation now working

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
+use std::fs::OpenOptions;
 fn main() {
     let mut numbercreated = 0;
     let args: Vec<String> = env::args().skip(1).collect();
     for arg in args {
         println!("Creating {}", arg);
-        //TODO: CREATE FILE
+        let _file = OpenOptions::new().write(true).create_new(true).open(arg);
         numbercreated+=1;
     }
     println!("Done. Created {} files.",numbercreated);
