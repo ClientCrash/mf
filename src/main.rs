219b6202b6c86ad2d8b1ee65b0bebf5460319a66
Trequa <5308705+Trequa@users.noreply.github.com>
2022-03-29 19:41:00 +0200

    If file already exists, inform user

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use std::env;
 use std::fs::OpenOptions;
+use std::io::ErrorKind;
+
 fn main() {
     let mut numbercreated = 0;
     let args: Vec<String> = env::args().skip(1).collect();
+
     for arg in args {
         println!("Creating {}", arg);
-        let _file = OpenOptions::new().write(true).create_new(true).open(arg);
-        numbercreated += 1;
+        if let Err(error) = OpenOptions::new().write(true).create_new(true).open(&arg) {
+            if error.kind() == ErrorKind::AlreadyExists {
+                println!("File {} already exists.", arg);
+            }
+        } else {
+            numbercreated += 1;
+        }
     }
+
     println!("Done. Created {} file/s.", numbercreated);
 }
