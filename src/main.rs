ae2deee86a2cbfb1bb667045261ebf08c8d16aa8
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2022-05-15 14:20:36 +0200

    added options and new feature set

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,70 @@
 use std::env;
 use std::fs::OpenOptions;
+use std::fs;
+use std::io::Write;
 fn main() {
-    let mut numbercreated = 0;
-    let args: Vec<String> = env::args().skip(1).collect();
-    for arg in args {
-        println!("Creating {}", arg);
-        let _file = OpenOptions::new().write(true).create_new(true).open(arg);
-        numbercreated += 1;
+    let mut modifications = 0;
+    let mut args: Vec<String> = env::args().skip(1).collect();
+    if args.len()<1 {
+        return;
     }
-    println!("Done. Created {} file/s.", numbercreated);
+
+    let mode = args[0].clone();
+    args.remove(0);
+
+
+    if mode == "-c" {
+            
+        for arg in args {
+            println!("Creating {}", arg);
+            let _file = OpenOptions::new().write(true).create_new(true).open(arg);
+            modifications += 1;
+        }
+
+    }
+    else if mode == "-h" {
+        println!("mf <mode> [file ... file ... file]");
+        println!("modes:");
+        println!("-h: Help | -c Create file/s | -m Merge file/s | -r Delete file/s");
+        println!("!! If the mode is modify first file is target file name !!");
+    }
+
+    else if mode == "-r" {
+        for arg in args {
+            println!("Removing {}", arg);
+            fs::remove_file(arg).expect("File removal failed.");
+            modifications += 1;
+        }
+
+    }
+
+    else if mode == "-m" {
+        let target = args[0].clone();
+        args.remove(0);
+        let mut c= Vec::new();
+        for arg in args {
+            println!("Reading {}", arg);
+            let fc = fs::read_to_string(arg).expect("Read failed.");
+            c.push(fc);
+        }
+
+        println!("Writing {}", target);
+        modifications += 1;
+        
+
+        let mut file = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .append(true)
+        .open(target)
+        .unwrap();
+        for content in c {
+            write!(file,"{}",content).expect("Write failed.");
+        }
+
+
+
+    }
+
+    println!("Done. Created/Modified {} file/s.", modifications);
 }
